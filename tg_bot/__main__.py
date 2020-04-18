a583d7f06a633f27cf672a5faaad8e882efbd300
ringtonessquad <63552326+ringtonessquad@users.noreply.github.com>
2020-04-18 16:04:07 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,19 +18,14 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 Hi {}, my name is {}! 
-I am an Anime themed group management bot.
+Hello I'm A Group Manager. You Can Find The List Of Available Commands With /help.
 
-You can find the list of available commands with /help.
-
-[Saitama's Repo](github.com/AnimeKaizoku/SaitamaRobot) 
-See [Basic Configuration Checklist](t.me/OnePunchUpdates/29) on how to secure your groups.
-The support group chat is at @OnePunchSupport
+Managed By [My Master](https://t.me/Unknown_Hacker_X) 
 """
 
 HELP_STRINGS = """
 Hey there! My name is *{}*.
-I'm a Hero For Fun and help admins manage their groups with One Punch! Have a look at the following for an idea of some of \
-the things I can help you with.
+Hello Here Are My Help Commands 🤘
 
 *Main* commands available:
  - /start: start the bot
@@ -46,10 +41,7 @@ the things I can help you with.
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-DONATE_STRING = """Heya, glad to hear you want to donate!
-Saitama is hosted on one of Kaizoku's Servers and doesn't require any donations as of now but \
-You can donate to the original writer of the Base code, Paul
-There are two ways of supporting him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+DONATE_STRING = """ Sorry I Don't Accept Donations 🤘 ."""
 
 IMPORTED = {}
 MIGRATEABLE = []
